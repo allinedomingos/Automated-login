3514715b532a0f752d86cbac8c9c8c8e3a189377
Alline Domingos <lline.domingosdmg@gmail.com>
2020-04-26 23:35:05 -0300

    Create Constants.java

---
+++ src/test/java/com/katalon/kata/sample/Constants.java
@@ -0,0 +1,8 @@
+package com.katalon.kata.sample;
+
+public class Constants {
+
+  public static String baseUrl = "https://www.netflix.com/br-en/";
+  public static String username = "seuemail@gmail.com";
+  public static String password = "suasenha";
+}
